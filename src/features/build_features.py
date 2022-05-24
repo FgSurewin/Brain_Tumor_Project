5906024555484e789ecf02125a312a617fb2d406
guerrerj97 <jguerre008@citymail.cuny.edu>
2022-05-24 13:12:15 -0400

    PCA added to src

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -5,6 +5,8 @@ import numpy as np
 from skimage.color import rgb2gray
 import cv2
 
+from sklearn.decomposition import PCA
+
 
 class build_features:
     def __init__(self, train_files, mask_files) -> None:
@@ -34,3 +36,11 @@ class build_features:
             image_dataset_np = np.array(image_dataset)
 
             image_df = pd.DataFrame(image_dataset_np)
+
+    def init_PCA(self, n_components=None):
+        if n_components is not None:
+            pca = PCA(n_components=n_components)
+        else:
+            pca = PCA()
+
+        return pca.fit_transform(self.df)
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,12 +1,29 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class visualize:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, pca, pca_reduced, df) -> None:
+        self.pca = pca
+        self.pca_reduced = pca_reduced
+        self.df = df
 
-    def get_length_of_classes():
-        sns.countplot(data=df, x=df['label'])
+    def get_length_of_classes(self):
+        sns.countplot(data=self.df, x=self.df['label'])
         plt.title('Length Of Classes')
+        plt.show()
+
+    def get_explained_variance(self):
+        plt.grid()
+        plt.plot(np.cumsum(self.pca.explained_variance_ratio_*100))
+        plt.xlabel('Number of components')
+        plt.ylabel('Explained variance')
+        plt.show()
+
+    def view_pca_with_n_components(self):
+        pca_recovered = self.pca.inverse_transform(self.pca_reduced)
+        img_pca_10 = pca_recovered[1, :].reshape([256, 256])
+        plt.imshow(img_pca_10, cmap='gray_r')
+        plt.title('Compressed image with n components', fontsize=15, pad=15)
         plt.show()
